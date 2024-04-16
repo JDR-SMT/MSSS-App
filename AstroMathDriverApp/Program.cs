417c919dd1e48d3116b4941668285d7802dfceb4
JDR-SMT <30058666@tafe.wa.edu.au>
2024-04-16 14:05:55 +0800

    AstroMath.dll Driver Application

---
+++ AstroMathDriverApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using AstroMath;
+using System;
+
+namespace AstroMathDriverApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Formulas formulas = new Formulas();
+			Console.WriteLine("AstroMath.dll Driver Application");
+
+			Console.WriteLine("\nCalculate the star velocity when the observed wavelength is 500.1 nm and the wavelength at rest is 500.0 nm: ");
+			Console.WriteLine($"{Math.Round(formulas.StarVelocity(500.1, 500.0), 0)} m/s.");
+
+			Console.WriteLine("\nCalculate the star distance when the parallax angle is 0.547 arcsec: ");
+			Console.WriteLine($"{Math.Round(formulas.StarDistance(0.547), 2)} pc.");
+
+			Console.WriteLine("\nCalculate Kelvin when the temperature is 27 degrees Celsius: ");
+			Console.WriteLine($"{formulas.Kelvin(27)} K.");
+
+			Console.WriteLine("\nCalculate the event horizon when the mass of a black hole is 8.2 x 10^36 kg: ");
+			Console.WriteLine($"{formulas.EventHorizon(8.2 * Math.Pow(10, 36)):E1} m.");
+
+			Console.ReadLine();
+		}
+	}
+}
